69c5ea008c9a83344904c071939fb5f83d5bb89a
Daniel Carmona <dyc0822@dexcom.com>
2022-12-14 09:02:53 -0800

    First Commit - APIs with Spring Boot and JBA database

---
+++ src/main/kotlin/com/myspring/Reminder.kt
@@ -0,0 +1,18 @@
+package com.myspring
+
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+
+@Entity
+class Reminder (
+    @Column(nullable = false)
+    val name: String,
+    @Column(nullable = false)
+    val time: Long,
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Int?=null
+)
